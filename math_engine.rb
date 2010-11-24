a24b0cae607b9dc3b86a115640e3fafbbe3e1bdd
Michael Baldry <michael.baldry@uswitch.com>
2010-11-24 10:22:42 +0000

    Fixing gem

--- math_engine.rb
+++
@@ -1,6 +0,0 @@
-require 'rubygems'
-require 'lexr'
-
-require File.expand_path(File.join(File.dirname(__FILE__), 'lib', 'math_lexer'))
-require File.expand_path(File.join(File.dirname(__FILE__), 'lib', 'math_parser'))
-require File.expand_path(File.join(File.dirname(__FILE__), 'lib', 'math_engine'))
