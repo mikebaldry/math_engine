8974585eb7028142a94fc39bb730f5b41e2b2dde
Michael Baldry <mike.baldry@forward.co.uk>
2012-06-01 14:11:50 +0100

    Adding test for another case

--- spec/unit/lexer_spec.rb
+++ spec/unit/lexer_spec.rb
@@ -1,21 +1,21 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper'))
 
 describe "Lexing expressions" do
-  it "should have the correct ast for simple addition" do
+  it "should have the correct tokens for simple addition" do
     subject = MathEngine::Lexer.new("1 + 1")
     subject.next.should == Lexr::Token.number(1)
     subject.next.should == Lexr::Token.addition('+')
     subject.next.should == Lexr::Token.number(1)
   end
   
-  it "should have the correct ast for simple addition without whitespace" do
+  it "should have the correct tokens for simple addition without whitespace" do
     subject = MathEngine::Lexer.new("1+1")
     subject.next.should == Lexr::Token.number(1)
     subject.next.should == Lexr::Token.addition('+')
     subject.next.should == Lexr::Token.number(1)
   end
   
-  it "should have the correct ast for simple addition without whitespace" do
+  it "should have the correct tokens for simple addition without whitespace" do
     subject = MathEngine::Lexer.new("-10*(-3--2)")
     subject.next.should == Lexr::Token.number(-10)
     subject.next.should == Lexr::Token.multiplication("*")
@@ -25,6 +25,21 @@ describe "Lexing expressions" do
     subject.next.should == Lexr::Token.subtraction("-")
     subject.next.should == Lexr::Token.number(-2)
     subject.next.should == Lexr::Token.close_parenthesis(")")
+
+    subject.next.should == Lexr::Token.end
+  end
+  
+  it "should do this expression properly" do
+    subject = MathEngine::Lexer.new("(3+2)-2")
+  
+    subject.next.should == Lexr::Token.open_parenthesis("(")
+    subject.next.should == Lexr::Token.number(3)
+    subject.next.should == Lexr::Token.addition("+")
+    subject.next.should == Lexr::Token.number(2)
+    subject.next.should == Lexr::Token.close_parenthesis(")")
+
+    subject.next.should == Lexr::Token.subtraction("-")
+    subject.next.should == Lexr::Token.number(2)
 
     subject.next.should == Lexr::Token.end
   end
